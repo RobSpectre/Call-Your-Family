3dffdf638c7783fa29a7b5c6b616f6aab99a24eb
Rob Spectre <rob.spectre@gmail.com>
2012-06-11 19:47:02 -0500

    1) Changed templates. 2) Added web tests.

--- app.py
+++ app.py
@@ -13,6 +13,9 @@ from twilio.rest import TwilioRestClient
 # Declare and configure application
 app = Flask(__name__, static_url_path='/static')
 app.config.from_pyfile('local_settings.py')
+# Instantiate Twilio REST client to use for sending SMS.
+app.twilio_client = TwilioRestClient(app.config['TWILIO_ACCOUNT_SID'],
+            app.config['TWILIO_AUTH_TOKEN'])
 
 
 '''
@@ -20,23 +23,27 @@ Index Page
 Show a simple web form to submit your phone number and receive an
 SMS message.
 '''
+
+
 @app.route('/', methods=['GET', 'POST'])
 def index():
+    # Check if this is a submitted form, if so send the submitted
+    # phone number an SMS invite.
     if request.method == 'POST':
-        # Check if this is a submitted form, if so send the submitted
-        # phone number an SMS invite.
-        client = TwilioRestClient(app.config['ACCOUNT_SID'],
-            app.config['AUTH_TOKEN'])
-        client.sms.messages.create(from_=app.config['TWILIO_ACCOUNT_SID'],
-                to=request.form['phone_number'], body="Download Plants vs. "\
-                        "Zombies now using this link: %s" % \
-                        url_for('.invite', external=True))
+        # Send text message.
+        app.twilio_client.sms.messages.create(
+            from_=app.config['TWILIO_CALLER_ID'],
+            to=request.form['phone_number'], body="Download Plants vs. "\
+                    "Zombies now using this link: %s" % \
+                    url_for('.invite', external=True))
+        # Render Thank You page.
         params = {
             'phone_number': request.form['phone_number']}
         return render_template('thankyou.html', params=params)
     else:
         # If this is a GET, display the form to send the SMS invite.
         params = {
+            'sms_request_url': url_for('.sms', _external=True),
             'invite_uri': url_for('.invite', external=True)}
         return render_template('index.html', params=params)
 
@@ -46,6 +53,8 @@ Invite URI
 This is the link users are set to redirect them to the app in their platform's
 app store.
 '''
+
+
 @app.route('/invite')
 def invite():
     # If user is on iPhone/iPad, send to iTunes App Store
@@ -63,6 +72,8 @@ def invite():
 SMS Request URL
 This accepts incoming text messages and replies with a link to the app.
 '''
+
+
 @app.route('/sms', methods=['GET', 'POST'])
 def sms():
     response = twiml.Response()
--- tests/test_web.py
+++ tests/test_web.py
@@ -1,5 +1,11 @@
 import unittest
+from mock import patch
+
 from .context import app
+
+app.config['TWILIO_ACCOUNT_SID'] = 'ACxxxxxx'
+app.config['TWILIO_AUTH_TOKEN'] = 'yyyyyyyyy'
+app.config['TWILIO_CALLER_ID'] = '+15558675309'
 
 class WebTest(unittest.TestCase):
     def setUp(self):
@@ -13,7 +19,21 @@ class InviteTests(WebTest):
         self.assertTrue("/invite?external=True" in response.data,
                 "Did not get invite URI in response: %s" % response.data)
 
+    @patch('twilio.rest.resources.SmsMessages')
+    @patch('twilio.rest.resources.SmsMessage')
+    def test_invitePostGoodInput(self, MockMessage, MockMessages):
+        app.twilio_client.sms.messages = MockMessages.return_value
+        app.twilio_client.sms.messages.create = MockMessage.return_value
+        response = self.app.post('/',
+            data={'phone_number': '555-555-5555'})
+        self.assertEqual("200 OK", response.status)
+        app.twilio_client.sms.messages.create.assert_called_once_with(
+            from_=app.config['TWILIO_CALLER_ID'],
+            to="+15555555555",
+            body="Download Plants vs. Zombies now using this link: " \
+                "/invite?external=True")
 
-    def test_inviteGetWeb(self):
+    def test_invite(self):
         response = self.app.get('/invite')
         self.assertEqual("301 REDIRECT", response.status)
+
